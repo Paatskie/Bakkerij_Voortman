21e783e2dc276cabdd24b042257a5256fd37f06d
Paatskie <pennywiser@gmail.com>
2017-09-21 21:06:42 +0200

    Source bestanden voor development
    SRC map geupload voor development

---
+++ src/js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $(window).on('load scroll', function () {
+        var scrolled = $(this).scrollTop();
+        $('#title').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
+        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
+    });
+
+    // video controls
+    $('#state').on('click', function () {
+        var video = $('#hero-vid').get(0);
+        var icons = $('#state > span');
+        $('#overlay').toggleClass('fade');
+        if (video.paused) {
+            video.play();
+            icons.removeClass('fa-play').addClass('fa-pause');
+        } else {
+            video.pause();
+            icons.removeClass('fa-pause').addClass('fa-play');
+        }
+    });
+
+    //owl carousel / historie slide.bs.carousel
+    var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        margin: 10,
+        nav: true,
+        loop: true,
+        items: 1,
+        navText: [],
+        smartSpeed: 750
+      })
+});
+
+function init() {
+  window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 100,
+      header = document.querySelector("header");
+    if (distanceY > shrinkOn) {
+      classie.add(header,"smaller");
+    } else {
+      if (classie.has(header,"smaller")) {
+        classie.remove(header,"smaller");
+      }
+    }
+  });
+}
+window.onload = init();
