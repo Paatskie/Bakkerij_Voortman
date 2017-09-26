7d939733376fcb7ff907d92441a59c7fbcb1905a
Paatskie <pennywiser@gmail.com>
2017-09-26 20:35:00 +0200

    Desktop versie / iPad liggend klaar
    Verder met iPad staand en mobiel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
     $(window).on('load scroll', function () {
-        var scrolled = $(this).scrollTop();
-        $('#title').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
-        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
+      var scrolled = $(this).scrollTop();
+      $('#title').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
+      $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 1) + 'px, 0)'); // parallax (25% scroll rate)
     });
 
     // video controls
     $('#state').on('click', function () {
-        var video = $('#hero-vid').get(0);
-        var icons = $('#state > span');
-        $('#overlay').toggleClass('fade');
-        if (video.paused) {
-            video.play();
-            icons.removeClass('fa-play').addClass('fa-pause');
-        } else {
-            video.pause();
-            icons.removeClass('fa-pause').addClass('fa-play');
-        }
+      var video = $('#hero-vid').get(0);
+      var icons = $('#state > span');
+      $('#overlay').toggleClass('fade');
+      if (video.paused) {
+          video.play();
+          icons.removeClass('fa-play').addClass('fa-pause');
+      } else {
+          video.pause();
+          icons.removeClass('fa-pause').addClass('fa-play');
+      }
     });
 
     //owl carousel / historie slide.bs.carousel
     var owl = $('.owl-carousel');
-      owl.owlCarousel({
-        margin: 10,
-        nav: true,
-        loop: true,
-        items: 1,
-        navText: [],
-        smartSpeed: 750
-      })
+    owl.owlCarousel({
+      margin: 10,
+      nav: true,
+      loop: true,
+      items: 1,
+      navText: [],
+      smartSpeed: 750
+    })
 });
 
 function init() {
